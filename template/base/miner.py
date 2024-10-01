1055a23be3cf895163db215144e7523d00456885
Matt Young <my@nickel5.com>
2024-10-01 10:20:44 -0600

    emoji for resync logs

--- template/base/miner.py
+++ template/base/miner.py
@@ -206,7 +206,7 @@ class BaseMinerNeuron(BaseNeuron):
 
     def resync_metagraph(self):
         """Resyncs the metagraph and updates the hotkeys and moving averages based on the new metagraph."""
-        bt.logging.info("resync_metagraph()")
+        bt.logging.info("🔄 resync_metagraph()")
 
         # Sync the metagraph.
         self.metagraph.sync(subtensor=self.subtensor)
