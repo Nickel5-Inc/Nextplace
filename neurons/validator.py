0fd59823b31aced2b53ac32b7b199ce9058d9ae1
Matt Young <my@nickel5.com>
2024-10-14 10:14:45 -0600

    setup for testing

--- neurons/validator.py
+++ neurons/validator.py
@@ -7,7 +7,7 @@ from nextplace.validator.nextplace_validator import RealEstateValidator
 
 
 def main(validator):
-    step = 1  # Initialize step
+    step = 999  # Initialize step
     current_thread = threading.currentThread().name
 
     while True:
@@ -25,9 +25,9 @@ def main(validator):
                 thread = threading.Thread(target=validator.manage_miner_data, name="📋 MinerRegistrationThread 📋")
                 thread.start()
             
-            if step % 200 == 0:  # Send predictions to website
-                thread = threading.Thread(target=validator.process_and_send_predictions, name="🛰️ WebsiteConnectionThread 🛰️")
-                thread.start()
+            # if step % 200 == 0:  # Send predictions to website
+            #     thread = threading.Thread(target=validator.process_and_send_predictions, name="🛰️ WebsiteConnectionThread 🛰️")
+            #     thread.start()
 
             if step >= 1000:  # Time to update scores and set weights
                 thread = threading.Thread(target=validator.scorer.run_score_predictions, name="🏋🏻 ScoreThread 🏋🏻")  # Create thread
