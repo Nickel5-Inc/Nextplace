efb33321fb5f9a541ec9f2e7c058d40108d5042f
Matt Young <my@nickel5.com>
2024-10-17 11:24:29 -0600

    remove refs to old predictions table

--- nextplace/validator/setting_weights/weights.py
+++ nextplace/validator/setting_weights/weights.py
@@ -40,26 +40,29 @@ class WeightSetter:
         """
         Adjust scores to zero for miners with fewer than 10 predictions in the last 5 days.
         """
-        # Get the recent date threshold
-        recent_date = (datetime.now(timezone.utc) - timedelta(days=5)).strftime(ISO8601)
+        # !!! IMPORTANT !!!
+        # If we go back to using this idea, need to get data for each miner at a time, from that miner's predictions table
 
-        # Query to get the count of recent predictions for each miner
-        query = f'''
-            SELECT miner_hotkey, COUNT(*) as recent_count
-            FROM predictions
-            WHERE score_timestamp > '{recent_date}'
-            GROUP BY miner_hotkey
-        '''
-        recent_counts = self.database_manager.query(query)
-
-        # Build a dictionary of miner_hotkey to recent_count
-        recent_counts_dict = {miner_hotkey: recent_count for miner_hotkey, recent_count in recent_counts}
-
-        # Set scores to 0 for miners with less than 10 recent predictions
-        for miner_hotkey, uid in hotkey_to_uid.items():
-            recent_count = recent_counts_dict.get(miner_hotkey, 0)
-            if recent_count < 8:
-                scores[uid] = 0
+        # # Get the recent date threshold
+        # recent_date = (datetime.now(timezone.utc) - timedelta(days=5)).strftime(ISO8601)
+        #
+        # # Query to get the count of recent predictions for each miner
+        # query = f'''
+        #     SELECT miner_hotkey, COUNT(*) as recent_count
+        #     FROM predictions
+        #     WHERE score_timestamp > '{recent_date}'
+        #     GROUP BY miner_hotkey
+        # '''
+        # recent_counts = self.database_manager.query(query)
+        #
+        # # Build a dictionary of miner_hotkey to recent_count
+        # recent_counts_dict = {miner_hotkey: recent_count for miner_hotkey, recent_count in recent_counts}
+        #
+        # # Set scores to 0 for miners with less than 10 recent predictions
+        # for miner_hotkey, uid in hotkey_to_uid.items():
+        #     recent_count = recent_counts_dict.get(miner_hotkey, 0)
+        #     if recent_count < 8:
+        #         scores[uid] = 0
 
     def calculate_miner_scores(self):
         try:  # database_manager lock is already acquire at this point
