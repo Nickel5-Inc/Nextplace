4e77a83fc746340181da6013e803fbf7bf2337d7
Matt Young <my@nickel5.com>
2024-10-18 08:47:14 -0600

    add data script

---
+++ data/extract_scored_predictions.py
@@ -0,0 +1,398 @@
+import sqlite3
+from typing import Tuple
+import csv
+
+db_path = f'validator_v1.db'  # Set db path
+
+real_estate_markets = [
+    {
+        "name": "Columbus",
+        "id": "6_4664"
+    },
+    {
+        "name": "Cincinnati",
+        "id": "6_3879"
+    },
+    {
+        "name": "Orlando",
+        "id": "6_13655"
+    },
+    {
+        "name": "Jacksonville",
+        "id": "6_8907"
+    },
+    {
+        "name": "Modesto",
+        "id": "6_12359"
+    },
+    {
+        "name": "Houston",
+        "id": "6_8903"
+    },
+    {
+        "name": "San Jose",
+        "id": "6_17420"
+    },
+    {
+        "name": "Fresno",
+        "id": "6_6904"
+    },
+    {
+        "name": "Long Beach",
+        "id": "6_10940"
+    },
+    {
+        "name": "Miami",
+        "id": "6_11458"
+    },
+    {
+        "name": "Henderson",
+        "id": "6_8147"
+    },
+    {
+        "name": "Los Angeles",
+        "id": "6_11203"
+    },
+    {
+        "name": "Philadelphia",
+        "id": "6_15502"
+    },
+    {
+        "name": "Memphis",
+        "id": "6_12260"
+    },
+    {
+        "name": "San Diego",
+        "id": "6_16904"
+    },
+    {
+        "name": "Summerville",
+        "id": "6_17959"
+    },
+    {
+        "name": "San Antonio",
+        "id": "6_16657"
+    },
+    {
+        "name": "Nashville",
+        "id": "6_13415"
+    },
+    {
+        "name": "Sacramento",
+        "id": "6_16409"
+    },
+    {
+        "name": "Phoenix",
+        "id": "6_14240"
+    },
+    {
+        "name": "San Bernardino",
+        "id": "6_16659"
+    },
+    {
+        "name": "Detroit",
+        "id": "6_5665"
+    },
+    {
+        "name": "Atlanta",
+        "id": "6_30756"
+    },
+    {
+        "name": "Chicago",
+        "id": "6_29470"
+    },
+    {
+        "name": "Charlotte",
+        "id": "6_3105"
+    },
+    {
+        "name": "Mesa",
+        "id": "6_11736"
+    },
+    {
+        "name": "Denver",
+        "id": "6_5155"
+    },
+    {
+        "name": "Tampa",
+        "id": "6_18142"
+    },
+    {
+        "name": "Corpus Christi",
+        "id": "6_35781"
+    },
+    {
+        "name": "San Francisco",
+        "id": "6_17151"
+    },
+    {
+        "name": "New York",
+        "id": "6_30749"
+    },
+    {
+        "name": "Dallas",
+        "id": "6_30794"
+    },
+    {
+        "name": "Indianapolis",
+        "id": "6_9170"
+    },
+    {
+        "name": "Washington",
+        "id": "6_12839"
+    },
+    {
+        "name": "Oklahoma City",
+        "id": "6_14237"
+    },
+    {
+        "name": "Las Vegas",
+        "id": "6_10201"
+    },
+    {
+        "name": "Louisville",
+        "id": "6_12262"
+    },
+    {
+        "name": "Spokane",
+        "id": "6_17154"
+    },
+    {
+        "name": "Austin",
+        "id": "6_30818"
+    },
+    {
+        "name": "Richmond",
+        "id": "6_17149"
+    },
+    {
+        "name": "Dayton",
+        "id": "6_5413"
+    },
+    {
+        "name": "Omaha",
+        "id": "6_9417"
+    },
+    {
+        "name": "Anaheim",
+        "id": "6_517"
+    },
+    {
+        "name": "Scottsdale",
+        "id": "6_16660"
+    },
+    {
+        "name": "Baton Rouge",
+        "id": "6_1336"
+    },
+    {
+        "name": "Minneapolis",
+        "id": "6_10943"
+    },
+    {
+        "name": "Greensboro",
+        "id": "6_7161"
+    },
+    {
+        "name": "Boise",
+        "id": "6_2287"
+    },
+    {
+        "name": "St. Louis",
+        "id": "6_16661"
+    },
+    {
+        "name": "Seattle",
+        "id": "6_16163"
+    },
+    {
+        "name": "Fort Worth",
+        "id": "6_30827"
+    },
+    {
+        "name": "Plano",
+        "id": "6_30868"
+    },
+    {
+        "name": "St. Petersburg",
+        "id": "6_16164"
+    },
+    {
+        "name": "Tucson",
+        "id": "6_19459"
+    },
+    {
+        "name": "Portland",
+        "id": "6_30772"
+    },
+    {
+        "name": "Winston-Salem",
+        "id": "6_19017"
+    },
+    {
+        "name": "Bakersfield",
+        "id": "6_953"
+    },
+    {
+        "name": "Milwaukee",
+        "id": "6_35759"
+    },
+    {
+        "name": "Reno",
+        "id": "6_15627"
+    },
+    {
+        "name": "Chandler",
+        "id": "6_3104"
+    },
+    {
+        "name": "Huntsville",
+        "id": "6_9408"
+    },
+    {
+        "name": "Colorado Springs",
+        "id": "6_4147"
+    },
+    {
+        "name": "Oakland",
+        "id": "6_13654"
+    },
+    {
+        "name": "Virginia Beach",
+        "id": "6_20418"
+    },
+    {
+        "name": "Irvine",
+        "id": "6_9361"
+    },
+    {
+        "name": "Gilbert",
+        "id": "6_6998"
+    },
+    {
+        "name": "Baltimore",
+        "id": "6_1073"
+    },
+    {
+        "name": "Tacoma",
+        "id": "6_17887"
+    },
+    {
+        "name": "Birmingham",
+        "id": "6_1823"
+    },
+    {
+        "name": "Fort Wayne",
+        "id": "6_6438"
+    },
+    {
+        "name": "Riverside",
+        "id": "6_15935"
+    },
+    {
+        "name": "Raleigh",
+        "id": "6_35711"
+    },
+    {
+        "name": "Lexington",
+        "id": "6_11746"
+    },
+    {
+        "name": "Chula Vista",
+        "id": "6_3494"
+    },
+    {
+        "name": "Garland",
+        "id": "6_30821"
+    },
+    {
+        "name": "Cleveland",
+        "id": "6_4145"
+    },
+    {
+        "name": "Kansas City",
+        "id": "6_35751"
+    },
+    {
+        "name": "Lancaster",
+        "id": "6_10233"
+    },
+    {
+        "name": "New Orleans",
+        "id": "6_14233"
+    },
+    {
+        "name": "Arlington",
+        "id": "6_21282"
+    },
+    {
+        "name": "Stockton",
+        "id": "6_19009"
+    }
+]
+
+def get_cursor() -> Tuple[sqlite3.Cursor, sqlite3.Connection]:
+    """
+    Get a cursor and connection reference from the database
+    Returns:
+        cursor & connection objects
+    """
+    db_connection = get_db_connection()
+    cursor = db_connection.cursor()
+    return cursor, db_connection
+
+
+def get_db_connection() -> sqlite3.Connection:
+    """
+    Get a reference to the database
+    Returns:
+        A database connection
+    """
+    return sqlite3.connect(db_path)
+
+
+def query(query_str: str) -> list[tuple]:
+    """
+    Get all results of a query from the database
+    Args:
+        query_str: a query string
+
+    Returns:
+        All rows matching the query
+    """
+    rows = []
+    cursor, db_connection = get_cursor()
+    try:
+        cursor.execute(query_str)
+        rows = cursor.fetchall()
+    finally:
+        cursor.close()
+        db_connection.close()
+        return rows
+
+
+if __name__ == "__main__":
+    query_str = """
+        SELECT nextplace_id, miner_hotkey, prediction_timestamp, predicted_sale_price, predicted_sale_date
+        FROM predictions
+        WHERE scored=1
+    """
+    results = query(query_str)
+    csv_data = []
+    for result in results:
+        entry = {
+            'nextplaceId': result[0],
+            'minerHotkey': result[1],
+            'minerColdkey': None,
+            'predictionDate': result[2],
+            'predictedSalePrice': result[3],
+            'predictedSaleDate': result[4]
+        }
+        csv_data.append(entry)
+
+    csv_file_name = 'scored_predictions.csv'
+
+    with open(csv_file_name, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=csv_data[0].keys())
+        writer.writeheader()  # Write the headers
+        writer.writerows(csv_data)  # Write the rows
