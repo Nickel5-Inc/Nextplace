b8bb376a6c369b0977601322a9163cac937992a8
Matt Young <my@nickel5.com>
2024-10-22 10:32:45 -0600

    update system.py

--- nextplace/validator/utils/system.py
+++ nextplace/validator/utils/system.py
@@ -1,35 +1,40 @@
 import functools
 import multiprocessing
-from bittensor import logging
+import bittensor as bt
+
 
 def timeout_with_multiprocess(seconds):
+
     def decorator(func):
+
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+
             def target_func(result_dict, *args, **kwargs):
                 try:
                     result_dict["result"] = func(*args, **kwargs)
                 except Exception as e:
                     result_dict["exception"] = e
-
             manager = multiprocessing.Manager()
             result_dict = manager.dict()
             process = multiprocessing.Process(
                 target=target_func, args=(result_dict, *args), kwargs=kwargs
             )
+
             process.start()
             process.join(seconds)
 
             if process.is_alive():
                 process.terminate()
                 process.join()
-                logging.warning(
+                bt.logging.warning(
                     f"Function '{func.__name__}' timed out after {seconds} seconds"
                 )
                 return None
 
             if "exception" in result_dict:
-                raise result_dict["exception"]
+                bt.logging.warning("Exception in setting weights")
+                return None
 
             return result_dict.get("result", None)
 
