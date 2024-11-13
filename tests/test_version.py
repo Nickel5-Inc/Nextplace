92b888a26fb395d12b8ac76a56c445c2418b3adc
Matt Young <my@nickel5.com>
2024-11-13 07:27:21 -0700

    test for version number

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,6 @@ import configparser
 import os
 
 if __name__ == "__main__":
-    print('hello world!')
     config_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'setup.cfg')
     config = configparser.ConfigParser()
     config.read(config_file_path)
