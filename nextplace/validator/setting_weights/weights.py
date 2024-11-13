9e5c6e139113ddc546aa00c23b13e1ffe21d49fd
Matt Young <my@nickel5.com>
2024-11-13 13:37:03 -0700

    clean up

--- nextplace/validator/setting_weights/weights.py
+++ nextplace/validator/setting_weights/weights.py
@@ -23,8 +23,7 @@ class WeightSetter:
         """
         now = datetime.now(timezone.utc)
         time_diff = now - self.timer
-        # return time_diff >= timedelta(hours=1)
-        return time_diff >= timedelta(minutes=1)
+        return time_diff >= timedelta(hours=1)
 
     def check_timer_set_weights(self) -> None:
         """
