320ce7d931518366c37640cc9834c7fec49e94a8
Matt Young <my@nickel5.com>
2024-11-13 13:40:34 -0700

    weight scaling updates

--- nextplace/validator/setting_weights/weights.py
+++ nextplace/validator/setting_weights/weights.py
@@ -46,7 +46,7 @@ class WeightSetter:
 
             for miner_hotkey, lifetime_score, last_update_timestamp, total_predictions in results:
                 if miner_hotkey in hotkey_to_uid:
-                    last_update_dt = datetime.fromisoformat(last_update_timestamp)
+                    last_update_dt = datetime.strptime(last_update_timestamp, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
                     time_diff = now - last_update_dt  # Calculate difference between now and last update
 
                     # Score Scaling
@@ -65,7 +65,7 @@ class WeightSetter:
                     '''
                     if total_predictions < 5:
                         bt.logging.trace(f"| {current_thread} | 🚩 Miner '{miner_hotkey}' has less than 5 predictions. Scaling their score.")
-                        lifetime_score = lifetime_score * 0.5
+                        lifetime_score = lifetime_score * 0.75
 
                     elif total_predictions < 10:
                         bt.logging.trace(f"| {current_thread} | 🚩 Miner '{miner_hotkey}' has less than 10 predictions. Scaling their score.")
@@ -85,7 +85,7 @@ class WeightSetter:
             return scores
 
         except Exception as e:
-            bt.logging.error(f"❗Error fetching miner scores: {str(e)}")
+            bt.logging.error(f" | {current_thread} |❗Error fetching miner scores: {str(e)}")
             return torch.zeros(len(self.metagraph.hotkeys))
 
     def calculate_weights(self, scores):
