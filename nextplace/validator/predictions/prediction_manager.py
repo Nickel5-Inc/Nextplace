7439a39a02233c6fc2736093020ec5008ddc212c
Matt Young <my@nickel5.com>
2024-12-08 10:26:42 -0700

    testing

--- nextplace/validator/predictions/prediction_manager.py
+++ nextplace/validator/predictions/prediction_manager.py
@@ -18,7 +18,7 @@ class PredictionManager:
         self.database_manager = database_manager
         self.metagraph = metagraph
 
-    def process_predictions(self, responses: list[tuple]) -> None:
+    def process_predictions(self, responses: List[Tuple[RealEstatePredictions, str]]) -> None:
         """
         Process predictions from the Miners
         Args:
@@ -28,32 +28,34 @@ class PredictionManager:
             None
         """
         current_thread = threading.current_thread().name
-
-        bt.logging.debug(f"| {current_thread} | 🪲 DEBUG Type of Responses: '{type(responses)}'")
-        for response in responses:
-            bt.logging.debug(f"| {current_thread} | 🪲 DEBUG Inner Type of Responses: '{type(response)}'")
-            bt.logging.debug(f"| {current_thread} | 🪲 DEBUG Type of tuple: ('{type(response[0])}', '{type(response[1])}')")
-
-        synapse_id = responses[0][1]
-
-        bt.logging.info(f"| {current_thread} | 📡 Processing Responses for synapse with ID: '{synapse_id}'")
+        bt.logging.info(f"| {current_thread} | 📡 Processing Responses")
 
         if responses is None or len(responses) == 0:
-            bt.logging.error(f'| {current_thread} | ❗No responses received')
+            bt.logging.error(f'| {current_thread} | ❗ No responses received')
             return
 
         current_utc_datetime = datetime.now(timezone.utc)
         timestamp = current_utc_datetime.strftime(ISO8601)
         valid_hotkeys = set()
 
-        extract_synapse_data_query = "SELECT nextplace_ids FROM synapse_ids WHERE synapse_id = ?"
-        delete_synapse_data_query = "DELETE FROM synapse_ids WHERE synapse_id = ?"
-        nextplace_ids_tuple = (synapse_id,)
-        with self.database_manager.lock:
-            valid_synapse_data = self.database_manager.query_with_values(extract_synapse_data_query, nextplace_ids_tuple)  # Extract synapse data from db
-            self.database_manager.query_and_commit_with_values(delete_synapse_data_query, nextplace_ids_tuple)  # Delete synapse data from db
-
         for idx, response in enumerate(responses):  # Iterate responses
+            synapse_id = response[1]
+            bt.logging.error(f"| {current_thread} | 🪲 DEBUG Processing response with synapse_id '{synapse_id}'")
+            extract_synapse_data_query = "SELECT nextplace_ids FROM synapse_ids WHERE synapse_id = ?"
+            nextplace_ids_tuple = (synapse_id,)
+            with self.database_manager.lock:
+                valid_synapse_data = self.database_manager.query_with_values(extract_synapse_data_query, nextplace_ids_tuple)  # Extract synapse data from db
+
+            if not valid_synapse_data or len(valid_synapse_data) == 0:
+                bt.logging.info(f"| {current_thread} | ❗ Found invalid synapse id: '{synapse_id}'")
+                return
+
+            valid_nextplace_ids_for_synapse = valid_synapse_data[0][0]
+            try:
+                nextplace_id_set = set(json.loads(valid_nextplace_ids_for_synapse))  # Ensure the string is valid JSON
+            except json.JSONDecodeError as e:
+                bt.logging.error(f"| {current_thread} | ❗ Failed to decode JSON: {e}")
+                return
 
             try:
                 miner_hotkey = self.metagraph.hotkeys[idx]
@@ -68,22 +70,11 @@ class PredictionManager:
                 replace_policy_data_for_ingestion: list[tuple] = []
                 ignore_policy_data_for_ingestion: list[tuple] = []
 
-                if not valid_synapse_data or len(valid_synapse_data) == 0:
-                    bt.logging.info(f"| {current_thread} | ❗ Found invalid synapse id: '{synapse_id}'")
-                    return
-
-                valid_nextplace_ids_for_synapse = valid_synapse_data[0][0]
-                try:
-                    nextplace_id_set = set(json.loads(valid_nextplace_ids_for_synapse))  # Ensure the string is valid JSON
-                except json.JSONDecodeError as e:
-                    bt.logging.error(f"| {current_thread} | ❗ Failed to decode JSON: {e}")
-                    return
-
                 for prediction in response[0].predictions:  # Iterate predictions in each response
 
                     # Ignore nextplace_id's that weren't sent in the original synapse
                     if prediction.nextplace_id not in nextplace_id_set:
-                        bt.logging.error(f"| {current_thread} | ❗ Found invalid nextplace_id for this synapse, ignoring")
+                        bt.logging.error(f"| {current_thread} | ❗ Found invalid nextplace_id for this synapse, ignoring prediction")
                         continue
 
                     # Only process valid predictions
@@ -114,6 +105,12 @@ class PredictionManager:
 
             except Exception as e:
                 bt.logging.error(f"| {current_thread} | ❗Failed to process prediction: {e}")
+
+        found_synapse_ids = [(x[1],) for x in responses]  # Build list of tuples representing every synapse_id found in responses
+        bt.logging.error(f"| {current_thread} | 🪲 DEBUG Removing data for the following synapse_id's from the database '{found_synapse_ids}'")
+        delete_synapse_data_query = "DELETE FROM synapse_ids WHERE synapse_id = ?"
+        with self.database_manager.lock:
+            self.database_manager.query_and_commit_many(delete_synapse_data_query, found_synapse_ids)  # Delete synapse data from db
 
         self._track_miners(valid_hotkeys)
 
