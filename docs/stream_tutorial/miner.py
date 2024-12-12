73dac4ca00d4ec38b45fde2bf8e63623e12fe118
Ubuntu <ubuntu@ip-172-31-9-237.ap-northeast-1.compute.internal>
2024-12-12 06:35:43 +0000

    Charmingtest

--- docs/stream_tutorial/miner.py
+++ docs/stream_tutorial/miner.py
@@ -14,6 +14,7 @@ from typing import List, Dict, Tuple, Union, Callable, Awaitable
 
 from protocol import StreamPrompting
 from config import get_config, check_config
+from neurons.real_estate import RealEstateSynapse
 
 
 class StreamMiner(ABC):
