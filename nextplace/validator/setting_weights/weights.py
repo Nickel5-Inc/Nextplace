5701d2da4ad8e6f046aa3b9b5c13af2b4954d866
Matt Young <my@nickel5.com>
2025-01-02 11:37:40 -0700

    working on calculate_weights

--- nextplace/validator/setting_weights/weights.py
+++ nextplace/validator/setting_weights/weights.py
@@ -225,7 +225,7 @@ class WeightSetter:
         for miner in miner_weights:
             weights[miner[0]] = miner[1]
 
-        bt.logging.info(f"| {current_thread} | ⚖️ Calculated weights: {weights}")
+        bt.logging.info(f"| {current_thread} | ⚖️ Calculated weights: {list(weights)}")
 
         try:
             uid = self.metagraph.hotkeys.index(self.wallet.hotkey.ss58_address)
