ab2672b4226b97f80d3a538a005c593a3e95bc7a
estatecat <udemycertscheap@gmail.com>
2025-01-16 00:48:20 +0200

    last of old modules

--- nextplace/validator/synapse/synapse_manager.py
+++ nextplace/validator/synapse/synapse_manager.py
@@ -4,16 +4,20 @@ import bittensor as bt
 from nextplace.protocol import RealEstateSynapse, RealEstatePrediction, RealEstatePredictions
 from nextplace.validator.database.database_manager import DatabaseManager
 from nextplace.validator.utils.contants import NUMBER_OF_PROPERTIES_PER_SYNAPSE
+from nextplace.validator.forward_web_request.retrieve_web_query import WebsiteRequestsChecker
 
 """
 Helper class manages creating Synapse objects
 """
 
-
 class SynapseManager:
-
     def __init__(self, database_manager: DatabaseManager):
         self.database_manager = database_manager
+        self.website_checker = WebsiteRequestsChecker(database_manager)
+
+    def sync_website_data(self) -> bool:
+        """Synchronize data from API to database"""
+        return self.website_checker.update_from_api()
 
     def get_synapse(self) -> RealEstateSynapse or None:
         """
@@ -21,9 +25,12 @@ class SynapseManager:
         Returns:
             A RealEstateSynapse to send to Miners, or None
         """
-
         current_thread = threading.current_thread().name
+        
         try:
+            # Get user property requests from website
+            self.sync_website_data()
+
             # Query to get the next round of properties
             retrieve_query = f'''
                 SELECT * FROM properties
@@ -34,6 +41,13 @@ class SynapseManager:
             if len(property_data) == 0:
                 return None
 
+            # Add data from website_requests table if available
+            web_request_data = self.website_checker.fetch_rows(limit=2)
+            if web_request_data:
+                bt.logging.info(f"| {current_thread} | ✨ Adding {len(web_request_data)} rows from website_requests table")
+                property_data.extend(web_request_data)
+
+            # Delete processed rows
             nextplace_id_index = 0
             row_ids = [row[nextplace_id_index] for row in property_data]  # Extract unique ID's
             formatted_ids = ','.join(f"'{str(nextplace_id)}'" for nextplace_id in row_ids)
@@ -43,8 +57,16 @@ class SynapseManager:
                 '''
             self.database_manager.query_and_commit(delete_query)  # Remove the retrieved rows from the database
 
-            outgoing_data = []
+            if web_request_data:
+                web_request_ids = [row[0] for row in web_request_data]
+                formatted_web_request_ids = ','.join(f"'{str(nextplace_id)}'" for nextplace_id in web_request_ids)
+                delete_web_requests_query = f'''
+                    DELETE FROM website_requests
+                    WHERE nextplace_id IN ({formatted_web_request_ids})
+                '''
+                self.database_manager.query_and_commit(delete_web_requests_query)
 
+            outgoing_data = []
             for property_datum in property_data:  # Iterate db responses
                 if property_datum:
                     try:
