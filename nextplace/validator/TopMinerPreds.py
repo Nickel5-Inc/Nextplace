af7ac8213a2ed43f2a541c9bbb8602de0628cbf0
Matt Young <my@nickel5.com>
2025-02-27 09:31:52 -0700

    pred job

--- nextplace/validator/TopMinerPreds.py
+++ nextplace/validator/TopMinerPreds.py
@@ -1,6 +1,8 @@
+import threading
 import time
 import requests
 import csv
+import bittensor as bt
 from nextplace.validator.database.database_manager import DatabaseManager
 
 API_KEY = "tao-44658c1e-7dce-41ef-ac6b-15942665440e:ef1cc958"
@@ -11,7 +13,9 @@ class TopMinerPreds:
         self.database_manager = database_manager
 
     def _store_preds(self):
+        thread_name = threading.current_thread().name
         hotkey = self._get_top_miner_hotkey()
+        bt.logging.info(f"| {thread_name} | Found top miner: '{hotkey}'")
         self._write_preds_to_disc(hotkey)
 
     def _get_top_miner_hotkey(self) -> str:
@@ -32,9 +36,13 @@ class TopMinerPreds:
             writer = csv.writer(file)
             writer.writerow(["nextplace_id", "predicted_sale_price", "predicted_sale_date"])
             writer.writerows(result)
+        thread_name = threading.current_thread().name
+        bt.logging.info(f"| {thread_name} | Finished writing to file")
 
     def run(self):
         # RUN IN THREAD
+        thread_name = threading.current_thread().name
         while True:
+            bt.logging.info(f"| {thread_name} | Getting top preds")
             self._store_preds()
             time.sleep(60 * 60 * 24)  # Sleep for 1 day
